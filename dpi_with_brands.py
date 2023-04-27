7fd449a978deecfe8871caa0ab6cd57a10a13411
Praneeth Ponnekanti <praneethcs23@gmail.com>
2023-04-27 15:30:04 +0530

    Update dpi_with_brands.py

--- dpi_with_brands.py
+++ dpi_with_brands.py
@@ -68,8 +68,8 @@ def main():
             table_data = []
             for i, url in enumerate(urls):
                 brand_name = brands[i] if i < len(brands) else ""
-                #table_data.append((brand_name, url))
-                table_data.append((brand_name,  st.markdown(f"### [{url}]({url})")))
+                table_data.append((brand_name, url))
+                #table_data.append((brand_name,  st.markdown(f"### [{url}]({url})")))
             df = pd.DataFrame(table_data, columns=["Brand Name", "Google Search URL"])
             st.table(df)
             #st.dataframe(table_data, columns=["Brand Name", "Google Search URL"], value=[f"[{url}]({url})" for _, url in table_data])
